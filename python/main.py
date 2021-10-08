b7074ad816c5e5a81fe12a86907caedb0f482605
Tyler Sheaffer <ty.sheaffer@gmail.com>
2021-10-08 11:47:57 -1000

    again

--- python/main.py
+++ python/main.py
@@ -2,6 +2,6 @@ import board
 import neopixel
 
 pixels = neopixel.NeoPixel(board.D18, 30)
-print("pixels before is #{pixels}")
+print("pixels before is", pixels)
 pixels[0] = (255, 0, 0)
-print("pixels after  is #{pixels}")
+print("pixels  after is", pixels)
