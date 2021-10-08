e4f823eedf0b9bbee5142dc681087790fb19f578
Tyler Sheaffer <ty.sheaffer@gmail.com>
2021-10-08 11:56:37 -1000

    tweak the pin

--- python/main.py
+++ python/main.py
@@ -1,7 +1,8 @@
 import board
 import neopixel
+from adafruit_blinka.microcontroller.generic_linux.libgpiod_pin import Pin
 
-pixels = neopixel.NeoPixel(board.D18, 30)
+pixels = neopixel.NeoPixel(Pin(12), 30)
 print("pixels before is", pixels)
 pixels[0] = (255, 0, 0)
 print("pixels  after is", pixels)
