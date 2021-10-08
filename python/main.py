931a7113c9c7b0a774442e37f5f203d925e4b094
Tyler Sheaffer <ty.sheaffer@gmail.com>
2021-10-08 12:44:33 -1000

    convert to hex

--- python/main.py
+++ python/main.py
@@ -4,13 +4,18 @@ import neopixel
 
 sleep_duration = 0.5
 
+def hex_to_rgb(value):
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i+lv//3], 16) for i in range(0, lv, lv//3))
+
 colors = [
-    '#d9ed92',
-    '#b5e48c',
-    '#99d98c',
-    '#34a0a4',
-    '#1a759f',
-    '#184e77',
+    hex_to_rgb('#d9ed92'),
+    hex_to_rgb('#b5e48c'),
+    hex_to_rgb('#99d98c'),
+    hex_to_rgb('#34a0a4'),
+    hex_to_rgb('#1a759f'),
+    hex_to_rgb('#184e77'),
     # [255, 0, 0],
     # [255, 255, 0],
     # [0, 255, 0],
