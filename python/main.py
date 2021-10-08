16144d4f2954b315e3870a439f8e56b43d5b4896
Tyler Sheaffer <ty.sheaffer@gmail.com>
2021-10-08 12:38:12 -1000

    faster, all primaries

--- python/main.py
+++ python/main.py
@@ -2,14 +2,31 @@ import time
 import board
 import neopixel
 
+sleep_duration = 0.5
+
+colors = [
+    [255, 0, 0],
+    [255, 255, 0],
+    [0, 255, 0],
+    [0, 255, 255],
+    [0, 0, 255],
+    [255, 0, 255],
+]
+
 pixels = neopixel.NeoPixel(board.D18, 50)
 def loopPrimaryColors():
-    pixels.fill((0, 255, 0))
-    time.sleep(2.0)
-    pixels.fill((255, 0, 0))
-    time.sleep(2.0)
-    pixels.fill((0, 0, 255))
-    time.sleep(2.0)
+    pixels.fill(colors[0])
+    time.sleep(sleep_duration)
+    pixels.fill(colors[1])
+    time.sleep(sleep_duration)
+    pixels.fill(colors[2])
+    time.sleep(sleep_duration)
+    pixels.fill(colors[3])
+    time.sleep(sleep_duration)
+    pixels.fill(colors[4])
+    time.sleep(sleep_duration)
+    pixels.fill(colors[5])
+    time.sleep(sleep_duration)
 
 while True:
     loopPrimaryColors()
