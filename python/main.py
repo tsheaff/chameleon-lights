472a9d14413b0c6e22023d430a94f8379ea7deeb
Tyler Sheaffer <ty.sheaffer@gmail.com>
2021-10-08 12:48:07 -1000

    better pallette

--- python/main.py
+++ python/main.py
@@ -2,7 +2,7 @@ import time
 import board
 import neopixel
 
-sleep_duration = 0.5
+sleep_duration = 0.2
 
 def hex_to_rgb(value):
     value = value.lstrip('#')
@@ -10,34 +10,32 @@ def hex_to_rgb(value):
     return tuple(int(value[i:i+lv//3], 16) for i in range(0, lv, lv//3))
 
 colors = [
-    hex_to_rgb('#d9ed92'),
-    hex_to_rgb('#b5e48c'),
-    hex_to_rgb('#99d98c'),
-    hex_to_rgb('#34a0a4'),
-    hex_to_rgb('#1a759f'),
-    hex_to_rgb('#184e77'),
-    # [255, 0, 0],
-    # [255, 255, 0],
-    # [0, 255, 0],
-    # [0, 255, 255],
-    # [0, 0, 255],
-    # [255, 0, 255],
+    '#10451d',
+    '#155d27',
+    '#1a7431',
+    '#208b3a',
+    '#25a244',
+    '#2dc653',
+    '#4ad66d',
+    '#6ede8a',
+    '#92e6a7',
+    '#b7efc5',
+    '#92e6a7',
+    '#6ede8a',
+    '#4ad66d',
+    '#2dc653',
+    '#25a244',
+    '#208b3a',
+    '#1a7431',
+    '#155d27',
+    '#10451d',
 ]
 
 pixels = neopixel.NeoPixel(board.D18, 50)
 def loopPrimaryColors():
-    pixels.fill(colors[0])
-    time.sleep(sleep_duration)
-    pixels.fill(colors[1])
-    time.sleep(sleep_duration)
-    pixels.fill(colors[2])
-    time.sleep(sleep_duration)
-    pixels.fill(colors[3])
-    time.sleep(sleep_duration)
-    pixels.fill(colors[4])
-    time.sleep(sleep_duration)
-    pixels.fill(colors[5])
-    time.sleep(sleep_duration)
+    for hex in colors:
+        pixels.fill(hex_to_rgb(hex))
+        time.sleep(sleep_duration)
 
 while True:
     loopPrimaryColors()
