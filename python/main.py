aacb06b4daa01758e896f6843e9858e4a7ead702
Tyler Sheaffer <ty.sheaffer@gmail.com>
2021-10-08 12:03:36 -1000

    try D12

--- python/main.py
+++ python/main.py
@@ -1,7 +1,7 @@
 import board
 import neopixel
 
-pixels = neopixel.NeoPixel(board.D10, 30)
+pixels = neopixel.NeoPixel(board.D12, 30)
 print("pixels before is", pixels)
 pixels.fill((0, 255, 0))
 print("pixels  after is", pixels)
