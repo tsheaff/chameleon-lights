9490bab1fe4aae5b4ea55e93af313b04f8252c92
Tyler Sheaffer <ty.sheaffer@gmail.com>
2021-10-08 12:01:57 -1000

    green and explicit show

--- python/main.py
+++ python/main.py
@@ -3,5 +3,6 @@ import neopixel
 
 pixels = neopixel.NeoPixel(board.D18, 30)
 print("pixels before is", pixels)
-pixels[0] = (255, 0, 0)
+pixels.fill((0, 255, 0))
 print("pixels  after is", pixels)
+pixels.show()
