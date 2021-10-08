e5d5af4b0a71fdf2074acf69799329abcd0d3c4c
Tyler Sheaffer <ty.sheaffer@gmail.com>
2021-10-08 12:33:30 -1000

    iteration

--- python/main.py
+++ python/main.py
@@ -1,9 +1,16 @@
+import time
 import board
 import neopixel
 
 pixels = neopixel.NeoPixel(board.D18, 30)
-print("pixels before is", pixels)
-pixels.fill((0, 255, 0))
-print("pixels  after is", pixels)
-pixels.show()
-pixels.write()
+def loopPrimaryColors():
+    pixels.fill((0, 255, 0))
+    time.sleep(1)
+    pixels.fill((255, 0, 0))
+    time.sleep(1)
+    pixels.fill((255, 0, 0))
+
+loopPrimaryColors()
+
+# pixels.show()
+# pixels.write()
