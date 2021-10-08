c5772efafe48f3535653241ce780abc8f3991d46
Tyler Sheaffer <ty.sheaffer@gmail.com>
2021-10-08 12:57:33 -1000

    new pallette

--- python/main.py
+++ python/main.py
@@ -13,25 +13,14 @@ def hex_to_rgb(value):
     return tuple(int(value[i:i+lv//3], 16) for i in range(0, lv, lv//3))
 
 colors = [
-    '#10451d',
-    '#155d27',
-    '#1a7431',
-    '#208b3a',
-    '#25a244',
-    '#2dc653',
-    '#4ad66d',
-    '#6ede8a',
-    '#92e6a7',
-    '#b7efc5',
-    '#92e6a7',
-    '#6ede8a',
-    '#4ad66d',
-    '#2dc653',
-    '#25a244',
-    '#208b3a',
-    '#1a7431',
-    '#155d27',
-    '#10451d',
+    '#ffbe0b',
+    '#fb5607',
+    '#ff006e',
+    '#8338ec',
+    '#3a86ff',
+    '#8338ec',
+    '#ff006e',
+    '#fb5607',
 ]
 
 pixels = neopixel.NeoPixel(PIN, NUM_LEDS)
