58c10fd0edddfa0a629d500ac674154233675f30
Tyler Sheaffer <ty.sheaffer@gmail.com>
2021-10-08 12:35:21 -1000

    loop again

--- python/main.py
+++ python/main.py
@@ -2,15 +2,17 @@ import time
 import board
 import neopixel
 
-pixels = neopixel.NeoPixel(board.D18, 30)
+pixels = neopixel.NeoPixel(board.D18, 50)
 def loopPrimaryColors():
     pixels.fill((0, 255, 0))
     time.sleep(2.0)
     pixels.fill((255, 0, 0))
     time.sleep(2.0)
     pixels.fill((0, 0, 255))
+    time.sleep(2.0)
 
-loopPrimaryColors()
+while True:
+    loopPrimaryColors()
 
 # pixels.show()
 # pixels.write()
