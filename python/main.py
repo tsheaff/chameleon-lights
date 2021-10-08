99a20ee5e170a1df54828374c6dd3de8a4928e2e
Tyler Sheaffer <ty.sheaffer@gmail.com>
2021-10-08 11:57:08 -1000

    Revert "tweak the pin"
    This reverts commit e4f823eedf0b9bbee5142dc681087790fb19f578.

--- python/main.py
+++ python/main.py
@@ -1,8 +1,7 @@
 import board
 import neopixel
-from adafruit_blinka.microcontroller.generic_linux.libgpiod_pin import Pin
 
-pixels = neopixel.NeoPixel(Pin(12), 30)
+pixels = neopixel.NeoPixel(board.D18, 30)
 print("pixels before is", pixels)
 pixels[0] = (255, 0, 0)
 print("pixels  after is", pixels)
