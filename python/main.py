9b575db880b0ad246ce31dfa34ce57631d5fe923
Tyler Sheaffer <ty.sheaffer@gmail.com>
2021-10-08 23:12:18 -1000

    pulser

--- python/main.py
+++ python/main.py
@@ -13,7 +13,6 @@ FRAME_DURATION = (1 / FRAME_RATE)
 
 pixels = neopixel.NeoPixel(PIN, NUM_PIXELS, auto_write=False)
 pixel_colors = list(map(lambda x: Color("#000000"), [None] * NUM_PIXELS))
-print("pixel_colors", pixel_colors)
 start_time = time.time()
 
 def interpolate(num1, num2, elapsed):
@@ -33,31 +32,48 @@ def colorToRGB(color):
         math.floor(color.blue * 255),
     ]
 
-def updateFrame(time_elapsed):
-    print("update frame", time_elapsed)
+def updateFrame(duration_elapsed):
+    pulser.apply()
 
 def applyColors():
     for i, c in enumerate(pixel_colors):
         pixels[i] = colorToRGB(c)
     pixels.show()
 
-def applyGradient():
-    color1 = Color("#ff0000")
-    color2 = Color("#00ffff")
+def applyGradient(color1, color2):
     for i, c in enumerate(pixel_colors):
         pixel_colors[i] = interpolateColors(color1, color2, i / NUM_PIXELS)
 
-applyGradient()
-applyColors()
-print("pixels", pixels)
+class GradientPulser:
+    def __init__(self, period, color1, color2):
+        self.period = period
+        self._color1 = color1
+        self._color2 = color2
+
+    def start(self):
+        self.timeBegan = time.time()
+
+    def apply(self):
+        time_elapsed = time.time() - self.timeBegan
+        x = time_elapsed / self.period
+        current_amplitude = math.sin(x)
+        color1Now = interpolateColors(color1, color2, current_amplitude)
+        color2Now = interpolateColors(color2, color1, current_amplitude)
+        applyGradient(color1Now, color2Now)
+
+pulser = GradientPulser(3.0, Color('red'), Color('blue'))
+pulser.start()
 
 while True:
     current_time = time.time()
-    time_elapsed = current_time - start_time
+    duration_elapsed = current_time - start_time
 
-    updateFrame(time_elapsed)
+    updateFrame(duration_elapsed)
     applyColors()
 
-    frame_clock_time = time.time() - current_time
-    sleep_time = max(0, FRAME_DURATION - frame_clock_time)
-    time.sleep(sleep_time)
+    color1 = Color("#ff0000")
+    color2 = Color("#00ffff")
+
+    frame_cpu_duration = time.time() - current_time
+    sleep_duration = max(0, FRAME_DURATION - frame_cpu_duration)
+    time.sleep(sleep_duration)
