cfccb03d8c2d7d69a6fb40e431502371b5241cb5
Tyler Sheaffer <ty.sheaffer@gmail.com>
2021-10-08 18:16:37 -1000

    draw loop

--- python/main.py
+++ python/main.py
@@ -1,44 +1,41 @@
-import time
 import board
 import neopixel
+import time
+import colors
+import math
+import random
 
-FRAME_RATE = 24.0
-FRAME_DURATION = (1 / FRAME_RATE)
 PIN = board.D18
 NUM_LEDS = 50
 
-def hex_to_rgb(value):
-    value = value.lstrip('#')
-    lv = len(value)
-    return tuple(int(value[i:i+lv//3], 16) for i in range(0, lv, lv//3))
-
-colors = [
-    '#ffbe0b',
-    '#fb5607',
-    '#ff006e',
-    '#8338ec',
-    '#3a86ff',
-    '#8338ec',
-    '#ff006e',
-    '#fb5607',
-]
+FRAME_RATE = 60.0
+FRAME_DURATION = (1 / FRAME_RATE)
 
 pixels = neopixel.NeoPixel(PIN, NUM_LEDS)
 
-# def parity_colors(even_color, odd_color):
-#     for i in range(NUM_LEDS):
-#         is_even = (i % 2) == 0
-#         if is_even:
-#             color = even_color
-#         else:
-#             color = odd_color
-#         pixels[i] = color
+start_time = time.time()
 
-def loopPrimaryColors():
-    for hex in colors:
-        for i in range(NUM_LEDS):
-            pixels[i] = hex_to_rgb(hex)
-            time.sleep(3 * FRAME_DURATION)
+def periodIndex(period, time_elapsed):
+    return math.floor(time_elapsed / period)
+
+red = 0
+green = 0
+blue = 0
+
+def drawFrame(time_elapsed):
+    global red, green, blue
+    colorIndex = periodIndex(0.1, time_elapsed)
+    distance = 3
+    red += random.randint(-distance, distance)
+    green += random.randint(-distance, distance)
+    blue += random.randint(-distance, distance)
+    pixels.fill([red % 256, green % 256, blue % 256])
+    # for hex in colors.pallette:
+    #     for i in range(NUM_LEDS):
+    #         pixels[i] = colors.hex_to_rgb(hex)
 
 while True:
-    loopPrimaryColors()
+    current_time = time.time()
+    time_elapsed = current_time - start_time
+    drawFrame(time_elapsed)
+    time.sleep(FRAME_DURATION)
