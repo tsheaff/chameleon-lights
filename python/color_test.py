a98844dfdb786bfe656781263e5257dea59a936d
Tyler Sheaffer <ty.sheaffer@gmail.com>
2021-10-14 10:51:31 -1000

    autoshow

--- python/color_test.py
+++ python/color_test.py
@@ -10,9 +10,13 @@ PIXELS_PER_STRAND = 50
 NUM_STRANDS = 1
 NUM_PIXELS = PIXELS_PER_STRAND * NUM_STRANDS
 
-pixels = neopixel.NeoPixel(PIN, NUM_PIXELS, auto_write=False)
+pixels = neopixel.NeoPixel(PIN, NUM_PIXELS, auto_write=True)
 pixel_colors = list(map(lambda x: Color("#000000"), [None] * NUM_PIXELS))
 
 while True:
-    pixels.fill(helpers.color_to_rgb(helpers.random_color()))
+    print("in the loop")
+    color = helpers.random_color()
+    print("color is", color)
+    print("rgb color is", helpers.color_to_rgb(color))
+    pixels.fill(helpers.color_to_rgb(color))
     time.sleep(2.0)
