33aa79f82e53001fcdc2d8a3d73e2c24e4563525
Tyler Sheaffer <ty.sheaffer@gmail.com>
2021-10-14 16:13:11 -1000

    no daemon

--- pi/startup.sh
+++ pi/startup.sh
@@ -6,7 +6,7 @@ echo "CHAMELEON_LOGS_FILE contents 1 is $(cat $CHAMELEON_LOGS_FILE)"
 echo "CHAMELEON_LOGS_FILE contents 2 is $(cat $CHAMELEON_LOGS_FILE)"
 cd /home/pi/Code/chameleon-lights/python
 echo "$(ls -la1)"
-sudo python3 ./main.py >> $CHAMELEON_LOGS_FILE &
+sudo python3 ./main.py >> $CHAMELEON_LOGS_FILE
 echo "CHAMELEON_LOGS_FILE contents 3 is $(cat $CHAMELEON_LOGS_FILE)"
 sleep 3
 echo "CHAMELEON_LOGS_FILE contents 4 is $(cat $CHAMELEON_LOGS_FILE)"
