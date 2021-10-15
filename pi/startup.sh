4333597d8c445fab240380ceb3d7138a0f53b20e
Tyler Sheaffer <ty.sheaffer@gmail.com>
2021-10-14 16:31:05 -1000

    logs

--- pi/startup.sh
+++ pi/startup.sh
@@ -1,5 +1,7 @@
-echo "Inside startup.sh"
+echo "chameleon-lights: startup.sh: Beginning"
 export CHAMELEON_LOGS_FILE=/home/pi/Desktop/chameleon-logs.txt
 > $CHAMELEON_LOGS_FILE
 cd /home/pi/Code/chameleon-lights/python
+echo "chameleon-lights: startup.sh: inside dir $(pwd)"
+echo "chameleon-lights: startup.sh: about to run main.py script..."
 sudo python3 ./main.py &> $CHAMELEON_LOGS_FILE
