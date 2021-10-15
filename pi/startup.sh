1fb4232adc174c6acbff84f22103af8958adae2a
Tyler Sheaffer <ty.sheaffer@gmail.com>
2021-10-14 16:28:22 -1000

    trim startup

--- pi/startup.sh
+++ pi/startup.sh
@@ -1,12 +1,5 @@
 echo "Inside startup.sh"
 export CHAMELEON_LOGS_FILE=/home/pi/Desktop/chameleon-logs.txt
-echo "CHAMELEON_LOGS_FILE script is $CHAMELEON_LOGS_FILE"
-echo "CHAMELEON_LOGS_FILE contents 1 is $(cat $CHAMELEON_LOGS_FILE)"
 > $CHAMELEON_LOGS_FILE
-echo "CHAMELEON_LOGS_FILE contents 2 is $(cat $CHAMELEON_LOGS_FILE)"
 cd /home/pi/Code/chameleon-lights/python
-echo "$(ls -la1)"
 sudo python3 ./main.py &> $CHAMELEON_LOGS_FILE
-echo "CHAMELEON_LOGS_FILE contents 3 is $(cat $CHAMELEON_LOGS_FILE)"
-sleep 3
-echo "CHAMELEON_LOGS_FILE contents 4 is $(cat $CHAMELEON_LOGS_FILE)"
