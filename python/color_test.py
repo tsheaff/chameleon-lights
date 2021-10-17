022604b7a8acbbbe156270762f0d89c8067cfa59
Tyler Sheaffer <ty.sheaffer@gmail.com>
2021-10-16 20:05:37 -1000

    color test mod 2

--- python/color_test.py
+++ python/color_test.py
@@ -13,10 +13,16 @@ NUM_PIXELS = PIXELS_PER_STRAND * NUM_STRANDS
 pixels = neopixel.NeoPixel(PIN, NUM_PIXELS, auto_write=True)
 pixel_colors = list(map(lambda x: Color("#000000"), [None] * NUM_PIXELS))
 
-while True:
-    print("in the loop")
-    color = helpers.random_color()
-    print("color is", color)
-    print("rgb color is", helpers.color_to_rgb(color))
-    pixels.fill(helpers.color_to_rgb(color))
-    time.sleep(2.0)
+for i in range(NUM_PIXELS):
+    if (i % 2) is 0:
+        pixels[i] = helpers.color_to_rgb(Color('green'))
+    else:
+        pixels[i] = helpers.color_to_rgb(Color('blue'))
+
+# while True:
+#     print("in the loop")
+#     color = helpers.random_color()
+#     print("color is", color)
+#     print("rgb color is", helpers.color_to_rgb(color))
+#     pixels.fill(helpers.color_to_rgb(color))
+#     time.sleep(2.0)
