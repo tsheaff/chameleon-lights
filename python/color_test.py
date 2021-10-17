e9481d9c9f2c374052315fbb3bc40f897e840f38
Tyler Sheaffer <ty.sheaffer@gmail.com>
2021-10-16 20:14:51 -1000

    old color test

--- python/color_test.py
+++ python/color_test.py
@@ -13,32 +13,32 @@ NUM_PIXELS = PIXELS_PER_STRAND * NUM_STRANDS
 pixels = neopixel.NeoPixel(PIN, NUM_PIXELS, auto_write=False)
 pixel_colors = list(map(lambda x: Color("#000000"), [None] * NUM_PIXELS))
 
-def render_every_other(n):
-    for i in range(NUM_PIXELS):
-        if (i % n) is 0:
-            pixels[i] = helpers.color_to_rgb(Color('orange'))
-        else:
-            pixels[i] = helpers.color_to_rgb(Color('purple'))
-    pixels.show()
+# def render_every_other(n):
+#     for i in range(NUM_PIXELS):
+#         if (i % n) is 0:
+#             pixels[i] = helpers.color_to_rgb(Color('orange'))
+#         else:
+#             pixels[i] = helpers.color_to_rgb(Color('purple'))
+#     pixels.show()
 
-render_every_other(2)
-time.sleep(2.0)
-render_every_other(3)
-time.sleep(2.0)
-render_every_other(4)
-time.sleep(2.0)
-render_every_other(5)
-time.sleep(2.0)
-render_every_other(4)
-time.sleep(2.0)
-render_every_other(3)
-time.sleep(2.0)
-render_every_other(2)
+# render_every_other(2)
+# time.sleep(2.0)
+# render_every_other(3)
+# time.sleep(2.0)
+# render_every_other(4)
+# time.sleep(2.0)
+# render_every_other(5)
+# time.sleep(2.0)
+# render_every_other(4)
+# time.sleep(2.0)
+# render_every_other(3)
+# time.sleep(2.0)
+# render_every_other(2)
 
-# while True:
-#     print("in the loop")
-#     color = helpers.random_color()
-#     print("color is", color)
-#     print("rgb color is", helpers.color_to_rgb(color))
-#     pixels.fill(helpers.color_to_rgb(color))
-#     time.sleep(2.0)
+while True:
+    print("in the loop")
+    color = helpers.random_color()
+    print("color is", color)
+    print("rgb color is", helpers.color_to_rgb(color))
+    pixels.fill(helpers.color_to_rgb(color))
+    time.sleep(2.0)
