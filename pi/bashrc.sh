50415d2a8780490f4acc1e54c5a289f3a62b6db3
Tyler Sheaffer <1238684+tsheaff@users.noreply.github.com>
2023-08-12 13:24:51 -1000

    Revert "add all the helpful commands and stuff we did today"
    This reverts commit 01374d1f0bf018601634835d19d103f4f70b432c.

--- pi/bashrc.sh
+++
@@ -1,3 +0,0 @@
-alias cd_lights='cd ~/Code && cd chameleon-lights && cd python'
-alias run_lights='sudo python3 main.py'
-alias pull_and_run_lights='git pull; run_lights'
--- pi/ssh.sh
+++
@@ -1,6 +0,0 @@
-# bash aliases helpful from the macbook you're accessing the RPi from
-
-export RPI_IP='10.0.0.19' # this was the ip address for the pi on Mom's home wifi
-
-alias cd_lights='cd_code && cd tysheaff && cd chameleon-lights'
-alias rpi_ssh='ssh pi@$RPI_IP'
