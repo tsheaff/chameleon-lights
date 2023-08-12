a1de930959d667dbac6e717fa078c445af9a0cf1
Tyler Sheaffer <1238684+tsheaff@users.noreply.github.com>
2023-08-12 13:24:49 -1000

    Revert "add comment"
    This reverts commit fa4a3c1a7916e9ffa49d650304c25d4ff7e777c0.

--- pi/bashrc.sh
+++ pi/bashrc.sh
@@ -1,5 +1,3 @@
-# this is the bash on the rpi itself
-
 alias cd_lights='cd ~/Code && cd chameleon-lights && cd python'
 alias run_lights='sudo python3 main.py'
 alias pull_and_run_lights='git pull; run_lights'
